ee421a97e387be9d9248c70bb3c515bb20d65ab9
ananyakumar9 <83970418+ananyakumar9@users.noreply.github.com>
2021-06-01 14:35:09 +0530

    Delete Images.js

--- src/Component/Images/Images.js
+++
@@ -1,8 +0,0 @@
-export const ep1 = require('./ep1.jpg');
-export const ep2 = require('./ep2.jpg');
-export const ep3 = require('./ep3.jpg');
-export const ep4 = require('./ep4.jpg');
-export const ep5 = require('./ep5.jpg');
-export const ep6 = require('./ep6.jpg');
-
-export default ep1;
